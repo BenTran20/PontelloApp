0605f9e43e6615f5b435578a5b9e9a98360090f2
lxvelyalina <imamalialina@gmail.com>
2026-03-07 23:19:27 -0500

    Delete PontelloApp/PontelloApp/ViewModels/EmailAddress.cs

--- PontelloApp/PontelloApp/ViewModels/EmailAddress.cs
+++
@@ -1,8 +0,0 @@
-namespace PontelloApp.ViewModels
-{
-    public class EmailAddress
-    {
-        public string Name { get; set; } = "";
-        public string Address { get; set; } = "";
-    }
-}
