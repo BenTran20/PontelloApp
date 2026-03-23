dbf9f70d391deca9f3b300472a5c0f5b20e6fe56
Dwayne-apples <alcaladwyane1@gmail.com>
2026-03-22 22:58:10 -0400

    Add ScheduledEmail.cs to Models

---
+++ PontelloApp/PontelloApp/Models/ScheduledEmail.cs
@@ -0,0 +1,17 @@
+﻿namespace PontelloApp.Models
+{
+    public class ScheduledEmail
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string HtmlBody { get; set; }
+        public byte[] AttachmentBytes { get; set; }
+        public string AttachmentName { get; set; }
+        public DateTime NextSendAt { get; set; }
+        public TimeSpan RepeatInterval { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
