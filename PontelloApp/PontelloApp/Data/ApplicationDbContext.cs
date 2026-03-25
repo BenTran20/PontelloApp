a919dde3a709903cba5b26f8ac5134edc381177b
trinhtina2006 <huyentrinh812006@gmail.com>
2026-03-25 16:05:30 -0400

    Add missing ScheduledEmail model reference

--- PontelloApp/PontelloApp/Data/ApplicationDbContext.cs
+++ PontelloApp/PontelloApp/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using PontelloApp.Models;
 
 namespace PontelloApp.Data
 {
