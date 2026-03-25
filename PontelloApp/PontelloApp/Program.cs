7ebd15aa560335eba98eab634188a11a5915fa0b
lxvelyalina <imamalialina@gmail.com>
2026-03-25 16:36:20 -0400

    Updated Program.cs

--- PontelloApp/PontelloApp/Program.cs
+++ PontelloApp/PontelloApp/Program.cs
@@ -54,11 +54,7 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddSession();
-
 var app = builder.Build();
-
-app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
