dd1c975cc32cbd75fd7b31cffb2ea0dfb2b1f7f9
trinhtina2006 <huyentrinh812006@gmail.com>
2026-03-25 16:04:55 -0400

    Refactor EmailSchedulerService for clarity

--- PontelloApp/PontelloApp/Ultilities/EmailSchedulerService.cs
+++ PontelloApp/PontelloApp/Ultilities/EmailSchedulerService.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
