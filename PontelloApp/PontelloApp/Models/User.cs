b7c1dff271b2ad2c0de4ed0e0ad93710d32d6d9a
trinhtina2006 <huyentrinh812006@gmail.com>
2026-03-26 12:32:15 -0400

    Create User model with identity and properties
    Added User class inheriting from IdentityUser with properties for FirstName, LastName, PhoneNumber, BINorEIN, and a collection of Orders.

---
+++ PontelloApp/PontelloApp/Models/User.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Identity;
+using Org.BouncyCastle.Bcpg;
+
+namespace PontelloApp.Models
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+        public string? BINorEIN { get; set; }
+
+        public ICollection<Order>? Orders { get; set; } = new HashSet<Order>();
+
+    }
+}
