2b072dd530d9a811f3818dcdee4837268d0f2163
trinhtina2006 <huyentrinh812006@gmail.com>
2026-03-26 12:39:26 -0400

    Refactor CartCountViewComponent to use UserManager
    Updated CartCountViewComponent to use UserManager for fetching the logged-in user and modified order retrieval logic accordingly.

--- PontelloApp/PontelloApp/Views/Shared/Components/CartCountViewComponent.cs
+++ PontelloApp/PontelloApp/Views/Shared/Components/CartCountViewComponent.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PontelloApp.Data;
@@ -6,19 +7,27 @@ using PontelloApp.Models;
 public class CartCountViewComponent : ViewComponent
 {
     private readonly PontelloAppContext _context;
+    private readonly UserManager<User> _userManager;
 
-    public CartCountViewComponent(PontelloAppContext context)
+
+    public CartCountViewComponent(PontelloAppContext context, UserManager<User> userManager)
     {
         _context = context;
+        _userManager = userManager;
     }
 
     public async Task<IViewComponentResult> InvokeAsync()
     {
-        int dealerId = 1; // TODO: replace with logged-in dealer
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+
+        if (user == null)
+        {
+            return View(0);
+        }
 
         var order = await _context.Orders
             .Include(o => o.Items)
-            .FirstOrDefaultAsync(o => o.DealerId == dealerId && o.Status == OrderStatus.Draft);
+            .FirstOrDefaultAsync(o => o.UserId == user.Id && o.Status == OrderStatus.Draft);
 
         int count = order?.Items?.Sum(i => i.Quantity) ?? 0;
 
