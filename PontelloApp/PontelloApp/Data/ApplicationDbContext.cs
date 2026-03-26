d5ef2a62a7b0ab5d9a7a68b5a4fc6156df1e53b4
trinhtina2006 <huyentrinh812006@gmail.com>
2026-03-26 12:32:42 -0400

    Change IdentityDbContext to use User class

--- PontelloApp/PontelloApp/Data/ApplicationDbContext.cs
+++ PontelloApp/PontelloApp/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using PontelloApp.Models;
 
 namespace PontelloApp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
