6cf0ebe0bf421b58bff13fadaf3e5a81c0ab46cd
Dwayne-apples <alcaladwyane1@gmail.com>
2026-03-30 00:04:20 -0400

    Update ScheduledEmail.cs

--- PontelloApp/PontelloApp/Models/ScheduledEmail.cs
+++ PontelloApp/PontelloApp/Models/ScheduledEmail.cs
@@ -9,12 +9,10 @@
         public string Email { get; set; }
         public string Subject { get; set; }
         public string HtmlBody { get; set; }
-        public byte[] AttachmentBytes { get; set; }
-        public string AttachmentName { get; set; }
+        public byte[]? AttachmentBytes { get; set; }
+        public string? AttachmentName { get; set; }
         public DateTime NextSendAt { get; set; }
-        public TimeSpan RepeatInterval { get; set; }
-
-        public bool IsActive { get; set; }
+        public string Remninder { get; set; }
         public bool PaymentTime { get; set; }
     }
 }
