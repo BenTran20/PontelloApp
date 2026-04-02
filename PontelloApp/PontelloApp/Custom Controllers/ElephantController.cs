5d3a905710f80122a837a312ecfe5f0b29de03a4
trinhtina2006 <huyentrinh812006@gmail.com>
2026-04-02 01:12:49 -0400

    Implement user notifications in ElephantController
    Added user notification retrieval in OnActionExecuting.

--- PontelloApp/PontelloApp/Custom Controllers/ElephantController.cs
+++ PontelloApp/PontelloApp/Custom Controllers/ElephantController.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.Filters;
+using PontelloApp.Data;
+using PontelloApp.Models;
 using PontelloApp.Ultilities;
 
 namespace PontelloApp.Custom_Controllers
@@ -16,6 +19,7 @@ namespace PontelloApp.Custom_Controllers
         //This is the list of Actions that will add the ReturnURL to ViewData
         internal string[] ActionWithURL = [ "Details", "Create", "Edit", "Delete", "Archive",
             "Add", "Update", "Remove" ];
+
         public override void OnActionExecuting(ActionExecutingContext context)
         {
             if (ActionWithURL.Contains(ActionName()))
@@ -26,6 +30,20 @@ namespace PontelloApp.Custom_Controllers
             {
                 //Clear the sort/filter/paging URL Cookie for Controller
                 CookieHelper.CookieSet(HttpContext, ControllerName() + "URL", "", -1);
+            }
+
+            if (User.Identity.IsAuthenticated)
+            {
+                var _context = (PontelloAppContext)HttpContext.RequestServices.GetService(typeof(PontelloAppContext));
+                var _userManager = (UserManager<User>)HttpContext.RequestServices.GetService(typeof(UserManager<User>));
+
+                var userId = _userManager.GetUserId(User);
+                var notifications = _context.Notifications
+                    .Where(n => n.UserId == userId)
+                    .OrderByDescending(n => n.CreatedAt)
+                    .ToList();
+
+                ViewData["Notifications"] = notifications;
             }
             base.OnActionExecuting(context);
         }
